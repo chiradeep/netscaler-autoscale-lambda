9c7cbd218e8aeabb40ba814eda15c483c47dda9f
Chiradeep Vittal <chiradeep@apache.org>
2016-12-09 17:45:51 -0800

    Default timeout is too low

--- setup/create_lambda.sh
+++ setup/create_lambda.sh
@@ -5,5 +5,6 @@ echo "Lambda ARN is $lambda_arn"
 LAMBDA_SUBNET_IDS=subnet-953f09e3
 LAMBDA_SECURITY_GROUP_IDS=sg-3c383945
 LAMBDA_FUNCTION_NAME=ConfigureNetScalerAutoScale
+TIMEOUT=90
 
-aws lambda create-function --function-name   $LAMBDA_FUNCTION_NAME  --zip-file fileb://${PWD}/bundle.zip --role $lambda_arn --handler handler.handler --runtime python2.7 --vpc-config SubnetIds=$LAMBDA_SUBNET_IDS,SecurityGroupIds=$LAMBDA_SECURITY_GROUP_IDS  --environment '{ "Variables": { "NS_URL": "$NS_URL", "NS_LOGIN": "$NS_LOGIN", "NS_PASSWORD": "$NS_PASSWORD", "S3_TFSTATE_BUCKET": "$S3_TFSTATE_BUCKET", "S3_TFCONFIG_BUCKET":"$S3_TFCONFIG_BUCKET" } }'
+aws lambda create-function --function-name   $LAMBDA_FUNCTION_NAME  --zip-file fileb://${PWD}/bundle.zip --role $lambda_arn --handler handler.handler --runtime python2.7 --vpc-config SubnetIds=$LAMBDA_SUBNET_IDS,SecurityGroupIds=$LAMBDA_SECURITY_GROUP_IDS  --timeout $TIMEOUT --environment '{ "Variables": { "NS_URL": "$NS_URL", "NS_LOGIN": "$NS_LOGIN", "NS_PASSWORD": "$NS_PASSWORD", "S3_TFSTATE_BUCKET": "$S3_TFSTATE_BUCKET", "S3_TFCONFIG_BUCKET":"$S3_TFCONFIG_BUCKET" } }'
