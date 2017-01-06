4a7c8d8905925174d9952ff0ed7cb15f7b1bdb62
Chiradeep Vittal <chiradeep@apache.org>
2017-01-06 15:39:16 -0800

    cleanup unused scripts

--- setup/delete-lambda-role.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-LAMBDA_ROLE_NAME=ConfigureNetScalerAutoScaleRole
-
-aws iam detach-role-policy --role-name $LAMBDA_ROLE_NAME --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
-aws iam detach-role-policy --role-name $LAMBDA_ROLE_NAME --policy-arn arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess
-aws iam detach-role-policy --role-name $LAMBDA_ROLE_NAME --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole
-
-aws iam delete-role --role-name $LAMBDA_ROLE_NAME
