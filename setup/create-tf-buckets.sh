8826d439a1d56062b4a5c064f83e6e9ed8d3f95d
Chiradeep Vittal <chiradeep@apache.org>
2017-01-06 15:39:00 -0800

    cleanup unused scripts

--- setup/create-tf-buckets.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-aws s3 mb s3://$S3_TFSTATE_BUCKET
-aws s3 mb s3://$S3_TFCONFIG_BUCKET
-
-aws s3api put-bucket-versioning --bucket  $S3_TFSTATE_BUCKET --versioning-configuration Status=Enabled
-
-
