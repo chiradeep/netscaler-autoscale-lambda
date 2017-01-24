3170772e97b1b42f6ec4ebeea1d32d7866ee4a70
Chiradeep Vittal <chiradeep@apache.org>
2017-01-23 16:21:14 -0800

    Handle launching in multiple zones

--- setup/vpc.tf
+++ setup/vpc.tf
@@ -4,13 +4,13 @@ module "vpc" {
   name = "${var.base_name}-ns-autoscale-vpc"
 
   cidr = "172.90.0.0/16"
-  private_subnets = ["172.90.1.0/24"]
-  public_subnets  = ["172.90.101.0/24"]
+  private_subnets = ["172.90.1.0/24", "172.90.11.0/24"]
+  public_subnets  = ["172.90.101.0/24", "172.90.111.0/24"]
 
   enable_nat_gateway = "true"
   map_public_ip_on_launch = "true"
   enable_dns_hostnames = "true"
   enable_dns_support = "true"
 
-  azs      = ["${data.aws_availability_zones.available.names[1]}"]
+  azs      = ["${data.aws_availability_zones.available.names[1]}", "${data.aws_availability_zones.available.names[0]}"]
 }
