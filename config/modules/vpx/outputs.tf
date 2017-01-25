55160c0eafc2b8b7bd749c89f45322dc09653ee1
Chiradeep Vittal <chiradeep@apache.org>
2017-01-25 15:19:32 -0800

    Document the VPX module

--- config/modules/vpx/outputs.tf
+++ config/modules/vpx/outputs.tf
@@ -5,6 +5,7 @@ output "launch_configuration" {
 output "asg_name" {
   value = "${aws_autoscaling_group.vpx-asg.id}"
 }
-output "client_security_group"{
-  value = "${aws_security_group.client_sg.id}" 
+
+output "client_security_group" {
+  value = "${aws_security_group.client_sg.id}"
 }
--- config/modules/vpx/resources.tf
+++ config/modules/vpx/resources.tf
@@ -1,36 +1,40 @@
 resource "aws_security_group" "client_sg" {
-    name = "client_sg"
-    ingress {
-      from_port = 80
-      to_port = 80
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-    ingress {
-      from_port = 443
-      to_port = 443
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress {
-       from_port = 0
-       to_port = 0
-       protocol = "-1"
-       cidr_blocks = ["0.0.0.0/0"]
-    }
-    vpc_id = "${var.vpc_id}"
+  name = "client_sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id = "${var.vpc_id}"
 }
 
-
 resource "aws_iam_instance_profile" "CitrixNodesProfile" {
-    name_prefix = "CitrixNodesProfile"
-    roles = ["${aws_iam_role.CitrixNodesInstanceRole.name}"]
+  name_prefix = "CitrixNodesProfile"
+  roles       = ["${aws_iam_role.CitrixNodesInstanceRole.name}"]
 }
 
 resource "aws_iam_role" "CitrixNodesInstanceRole" {
-    name_prefix = "CitrixNodesInstanceRole"
-    path = "/"
-    assume_role_policy = <<EOF
+  name_prefix = "CitrixNodesInstanceRole"
+  path        = "/"
+
+  assume_role_policy = <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -48,9 +52,10 @@ EOF
 }
 
 resource "aws_iam_role_policy" "Citrixnode" {
-    name = "Citrixnode"
-    role = "${aws_iam_role.CitrixNodesInstanceRole.id}"
-    policy = <<EOF
+  name = "Citrixnode"
+  role = "${aws_iam_role.CitrixNodesInstanceRole.id}"
+
+  policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -68,4 +73,3 @@ resource "aws_iam_role_policy" "Citrixnode" {
 }
 EOF
 }
-
