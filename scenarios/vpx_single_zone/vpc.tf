17002fda36263dee9e74f8a67a88eb5d9b599325
Chiradeep Vittal <chiradeep@apache.org>
2017-01-25 16:52:44 -0800

    Get autoscale working - need to enable features first

--- scenarios/vpx_single_zone/vpc.tf
+++ scenarios/vpx_single_zone/vpc.tf
@@ -3,14 +3,14 @@ module "vpc" {
 
   name = "${var.base_name}-ns-autoscale-vpc"
 
-  cidr = "172.90.0.0/16"
+  cidr            = "172.90.0.0/16"
   private_subnets = ["172.90.1.0/24"]
   public_subnets  = ["172.90.101.0/24"]
 
-  enable_nat_gateway = "true"
+  enable_nat_gateway      = "true"
   map_public_ip_on_launch = "true"
-  enable_dns_hostnames = "true"
-  enable_dns_support = "true"
+  enable_dns_hostnames    = "true"
+  enable_dns_support      = "true"
 
-  azs      = ["${data.aws_availability_zones.available.names[1]}"]
+  azs = ["${data.aws_availability_zones.available.names[1]}"]
 }
